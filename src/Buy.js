e393292e6f6d242b4a2dab549b2b01d5c24d4360
Yemezzy <ayubayelmee222@gmail.com>
2024-06-21 22:02:37 +0100

    first commit

--- src/Buy.js
+++ src/Buy.js
@@ -204,8 +204,10 @@ const Buy = () => {
           </div>
 
           <div className="mt-5 md:mt-10 flex gap-2 justify-center items-center">
-            <p className='text-sm font-bold uppercase'>Inquiries:</p>
-            <FaTelegram className="text-2xl text-[#1BB8D8]" />
+            <p className="text-sm font-bold uppercase">Inquiries:</p>
+            <Link to="https://t.me/dave1289">
+              <FaTelegram className="text-2xl text-[#1BB8D8]" />
+            </Link>
           </div>
         </section>
 
