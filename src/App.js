9f34855852f50c5e7646e94199b755af3c7e2823
Yemezzy <ayubayelmee222@gmail.com>
2025-05-18 18:25:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
 <Routes>
-  <Route path='/' element={<Startpage/>}/>
-  <Route path='/buy-dextbot' element={<Buy/>}/>
+  {/* <Route path='/' element={<Startpage/>}/> */}
+  {/* <Route path='/buy-dextbot' element={<Buy/>}/> */}
 </Routes>
 
 
